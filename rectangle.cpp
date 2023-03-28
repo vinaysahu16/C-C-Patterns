1b3b1044c617ef755171748f2aaa8d6ef8a44a72
Vinay Sahu <74305356+vinaysahu16@users.noreply.github.com>
2023-03-28 23:09:13 +0530

    Create rectangle.cpp

---
+++ rectangle.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    
+      int row,col;
+       for(row=0;row<3;row++){
+           for(col=0;col<5;col++){
+               cout<<"*"<< "  ";
+           }
+           cout<<endl;
+       } 
+    return 0;
+}
+
+
+
+
+output:-   *  *  *  *  *  
+           *  *  *  *  *  
+           *  *  *  *  *
